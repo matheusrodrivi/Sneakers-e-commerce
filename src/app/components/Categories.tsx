8c1b9e02989f40b44b5f4c018292eaa42ffb5ad4
matheusrodrivi <matheusrovicontato@gmail.com>
2026-02-10 20:40:21 -0300

    Updated: Logic and tailwind styles in Categories and ProductCard

--- src/app/components/Categories.tsx
+++ src/app/components/Categories.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ShoppingBasket } from "lucide-react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import path from "path";
 
 const catogories = [
   {
@@ -33,7 +34,16 @@ const catogories = [
 
 const Categories = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const selectedCategory = searchParams.get("category");
+
+  const handleChange = (value: string | null) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("category", value || "all");
+    router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg:gray-100 p-2 rounded-lg mb-4 text-sm">
@@ -41,6 +51,7 @@ const Categories = () => {
         <div
           className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${category.slug === selectedCategory ? "bg-white" : "text-gray-500"}`}
           key={category.name}
+          onClick={() => handleChange(category.slug)}
         >
           {category.icon}
           {category.name}
--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,9 +1,72 @@
 "use client";
 
+import Link from "next/link";
 import { ProductType } from "../types";
+import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
-  return <div className="">ProductCard</div>;
+  return (
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium"> {product.name} </h1>
+        <p className="text-sm"> {product.shortDescription} </p>
+
+        {/* SIZE */}
+        <div className="flex items-center gap-4 text-xs">
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500"> Size </span>
+            <select
+              name="size"
+              id="size"
+              className="ring ring-gray-300 rounded-md px-2 py-1"
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* COLORS */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500"> Colors </span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div className="" key={color}>
+                  <div
+                    className="w-[14px] h-[14px] rounded-full"
+                    style={{ backgroundColor: color }}
+                  ></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* PRICE? */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium"> {product.price.toFixed(2)} </p>
+          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+            <ShoppingCart className="w-4 h-4" />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
