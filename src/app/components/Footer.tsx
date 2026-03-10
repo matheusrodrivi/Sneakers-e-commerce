fd46566d3f184f11ee5c7de0b42aef50ff373b4e
matheusrodrivi <matheusrovicontato@gmail.com>
2026-03-10 13:23:11 -0300

    Updated: footer color and favicon.ico

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="mt-1 flex flex-col items-center gap-6 md:flex-row md:items-start md:justify-between md:gap-8 bg-[#eaf1f2] p-8 rounded-lg">
+    <div className="mt-1 flex flex-col items-center gap-6 md:flex-row md:items-start md:justify-between md:gap-8 bg-[#5b86ea] p-8 rounded-lg">
       <div className="flex flex-col gap-4 item-center md:items-start">
         <Link href="/" className="flex items-center justify-center gap-2">
           {/* <Image
@@ -16,24 +16,24 @@ const Footer = () => {
             SNEAKERS SHOP
           </p>
         </Link>
-        <p className="text-sm text-gray-400"> @ Sneakers Shop. </p>
-        <p className="text-sm text-gray-400"> All rights reseved. </p>
+        <p className="text-sm"> @ Sneakers Shop. </p>
+        <p className="text-sm"> All rights reseved. </p>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50 text-black"> Links </p>
-        <Link href="/"> Homepage </Link>
-        <Link href="/"> Contact </Link>
-        <Link href="/"> Terms of Service </Link>
-        <Link href="/"> Privacy Policy </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
+        <Link href="/" className="text-black"> Homepage </Link>
+        <Link href="/" className="text-black"> Contact </Link>
+        <Link href="/" className="text-black"> Terms of Service </Link>
+        <Link href="/" className="text-black"> Privacy Policy </Link>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50 text-black"> Links </p>
-        <Link href="/"> All Products </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
+        <Link href="/" className="text-black"> All Products </Link>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50 text-black"> Links </p>
-        <Link href="/"> About </Link>
-        <Link href="/"> Contact </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
+        <Link href="/" className="text-black"> About </Link>
+        <Link href="/" className="text-black"> Contact </Link>
       </div>
     </div>
   );
