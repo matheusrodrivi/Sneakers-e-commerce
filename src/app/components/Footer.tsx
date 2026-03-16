de121c8785327fab72f06b128ef4a7bacbd32f92
matheusrodrivi <matheusrovicontato@gmail.com>
2026-03-16 19:36:36 -0300

    Added: page.tsx (Cart) and new types

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="mt-1 flex flex-col items-center gap-6 md:flex-row md:items-start md:justify-between md:gap-8 bg-[#5b86ea] p-8 rounded-lg">
+    <div className="mt-1 flex flex-col items-center gap-6 md:flex-row md:items-start md:justify-between md:gap-8 bg-[#435057] p-8 rounded-lg">
       <div className="flex flex-col gap-4 item-center md:items-start">
         <Link href="/" className="flex items-center justify-center gap-2">
           {/* <Image
@@ -12,28 +12,28 @@ const Footer = () => {
             width={36}
             height={36}
           /> */}
-          <p className="hidden md:block text-md font-medium tracking-wider text-black">
+          <p className="hidden md:block text-md font-medium tracking-wider text-white">
             SNEAKERS SHOP
           </p>
         </Link>
-        <p className="text-sm"> @ Sneakers Shop. </p>
-        <p className="text-sm"> All rights reseved. </p>
+        <p className="text-sm text-white"> @ Sneakers Shop. </p>
+        <p className="text-sm text-white"> All rights reseved. </p>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
-        <Link href="/" className="text-black"> Homepage </Link>
-        <Link href="/" className="text-black"> Contact </Link>
-        <Link href="/" className="text-black"> Terms of Service </Link>
-        <Link href="/" className="text-black"> Privacy Policy </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-white"> LINKS </p>
+        <Link href="/" className="text-white"> Homepage </Link>
+        <Link href="/" className="text-white"> Contact </Link>
+        <Link href="/" className="text-white"> Terms of Service </Link>
+        <Link href="/" className="text-white"> Privacy Policy </Link>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
-        <Link href="/" className="text-black"> All Products </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-white"> LINKS </p>
+        <Link href="/" className="text-white"> All Products </Link>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="hidden md:block text-md font-medium tracking-wider text-black"> LINKS </p>
-        <Link href="/" className="text-black"> About </Link>
-        <Link href="/" className="text-black"> Contact </Link>
+        <p className="hidden md:block text-md font-medium tracking-wider text-white"> LINKS </p>
+        <Link href="/" className="text-white"> About </Link>
+        <Link href="/" className="text-white"> Contact </Link>
       </div>
     </div>
   );
